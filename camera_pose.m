a998916dd647297da4bdda8904e3260382853eb0
Yiangos01 <ygeorgiou12@gmail.com>
2018-04-15 03:09:31 +0200

    improvments part3

--- camera_pose.m
+++ camera_pose.m
@@ -7,8 +7,13 @@ Pcd_name2 = '0000000067.pcd';
 %cd '/home/yiangos/UvA/CV2/assignment/CV2';
 
 DOMnode = xmlread('/home/yiangos/UvA/CV2/assignment/Assignment 1 - v1.0.1/Assignment 1/Data/data/0000000001_camera.xml')
-camera = (-inv([1. 0. 0. ; 0. 1. 0. ; 0. 0. 1.])) * [6.26666665e-001 ; 6.26666665e-001 ; -1.25333369e-001] 
+camera = ((-inv([1. 0. 0. ; 0. 1. 0. ; 0. 0. 1.])) * [6.26666665e-001 ; 6.26666665e-001 ; -1.25333369e-001] )
 
+
+%X = u*Z/f;  
+%Y = v*Z/f,  
+%[525.  0. 320. ; 0. 525. 240. ; 0. 0. 1] *
+camera;    
 cloud_point1 = readPcd(strcat(Pcd_path,Pcd_name1));
 size(cloud_point1)
 %cloud_point2 = readPcd(strcat(Pcd_path,Pcd_name2));
@@ -21,7 +26,7 @@ size(cloud_point1)
 %[adjusted, pose] = ICP(cloud_point1(:,1:3)',cloud_point1(:,1:3)','all');
 
 %pose
-%scatter3(data(:,1),data(:,2),data(:,3),1,[1,0,0]);
+scatter3(cloud_point1(:,1),cloud_point1(:,2),cloud_point1(:,3),1,[1,0,0]);
 %adjusted = ICP(source,target,'all');
 
 %data.unpackRGBFloat
