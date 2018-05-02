1b5d09200ef1cf538487cf695e49a25c29f940c6
Yiangos01 <ygeorgiou12@gmail.com>
2018-05-02 15:00:36 +0200

    nothing

--- Assignment2/main.m
+++ Assignment2/main.m
@@ -1,10 +1,10 @@
 %% Read images
-imagefiles = dir('../../Assignment 2 - v1.0.1/Assignment 2/Data/House/House/*.png');  
+imagefiles = dir('/home/yiangos/UvA/CV2/assignment/CV2/Assignment2/Data/House/House/*.png');  
 image1_path = strcat(imagefiles(1).folder,'/',imagefiles(1).name);
 image2_path = strcat(imagefiles(1).folder,'/',imagefiles(2).name);
 image1 = imread(image1_path);
 image2 = imread(image2_path);
-
+% /home/yiangos/UvA/CV2/assignment/CV2/Assignment2/Data/House/House
 %% RANSAC
 [best_fit first_image_points, second_image_points] = RANSAC(image1_path,image2_path,3,1);
 
